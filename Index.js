d862c5987c97532742a4b48c23ce4a4e0665df6e
Aditya Sharma <aditya101sh@gmail.com>
2023-09-28 17:01:44 +0530

    edited server files

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
-const cors = require('cors'); // Import the cors middleware
+const cors = require("cors"); // Import the cors middleware
 
 const port = 3500; // You can choose any port you prefer
 
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // Define a route to retrieve engagement metrics for a YouTube video
-app.post('/api', async (req, res) => {
+app.post("/api", async (req, res) => {
   try {
     // Extract the video URL from the request body
     const videoUrl = req.body.videoUrl;
@@ -20,11 +20,11 @@ app.post('/api', async (req, res) => {
     const videoId = getVideoIdFromUrl(videoUrl);
 
     if (!videoId) {
-      return res.status(400).json({ error: 'Invalid YouTube video URL' });
+      return res.status(400).json({ error: "Invalid YouTube video URL" });
     }
 
     // Replace 'YOUR_API_KEY' with your actual YouTube Data API key
-    const apiKey = 'AIzaSyCJDYmlIj_h8ptrnmkAOlNZz0LU67xU9_w';
+    const apiKey = "AIzaSyCJDYmlIj_h8ptrnmkAOlNZz0LU67xU9_w";
 
     // Fetch data from the YouTube Data API for the video
     const videoApiUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoId}&key=${apiKey}`;
@@ -32,7 +32,7 @@ app.post('/api', async (req, res) => {
     const videoData = videoResponse.data.items[0];
 
     if (!videoData) {
-      return res.status(404).json({ error: 'Video not found' });
+      return res.status(404).json({ error: "Video not found" });
     }
 
     // Extract channel ID from video data
@@ -44,7 +44,7 @@ app.post('/api', async (req, res) => {
     const channelData = channelResponse.data.items[0];
 
     if (!channelData) {
-      return res.status(404).json({ error: 'Channel not found' });
+      return res.status(404).json({ error: "Channel not found" });
     }
 
     // Extract engagement metrics
@@ -54,34 +54,50 @@ app.post('/api', async (req, res) => {
     const views = videoData.statistics.viewCount;
 
     // Get the video's uploaded date from the snippet
-    const uploadedDate = videoData.snippet.publishedAt;
+    const uploadedDate = new Date(videoData.snippet.publishedAt);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const date = uploadedDate.toLocaleDateString('en-US', options);
+
+     // Access the video's thumbnails
+     const thumbnails = videoData.snippet.thumbnails;
+
+     // You can access thumbnails in different qualities, e.g., thumbnails.default.url
+     const thumbnailUrl = thumbnails.default.url;
+    
 
     // Get the video title from the snippet
     const videoTitle = videoData.snippet.title;
 
-    // Earnings from the video 
-    const earnings = calculateEarnings(subscriberCount, likes, comments, views).toLocaleString();
+    // Earnings from the video
+    const earnings = calculateEarnings(
+      subscriberCount,
+      likes,
+      comments,
+      views
+    ).toLocaleString();
 
-
-    // Return the engagement metrics and earning data as JSON 
+    // Return the engagement metrics and earning data as JSON
     res.json({
       earnings,
       subscriberCount,
       likes,
       comments,
       views,
-      uploadedDate,
-      videoTitle
+      date,
+      videoTitle,
+      thumbnailUrl
     });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
 // Helper function to extract video ID from a YouTube URL
 function getVideoIdFromUrl(url) {
-  const match = url.match(/(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?feature=player_embedded&v=))([\w-]{11})/);
+  const match = url.match(
+    /(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?feature=player_embedded&v=))([\w-]{11})/
+  );
   return match ? match[1] : null;
 }
 
@@ -90,9 +106,7 @@ function calculateEarnings(subscriberCount, likes, comments, views) {
   return Math.min(subscriberCount, views) + 10 * comments + 5 * likes;
 }
 
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
