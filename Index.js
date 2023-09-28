fd5a91bf17762500ab43b70d9a31bacf609fbeb6
Aditya Sharma <aditya101sh@gmail.com>
2023-09-28 14:40:30 +0530

    Server Code

---
+++ Index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const cors = require('cors'); // Import the cors middleware
+
+const port = 3500; // You can choose any port you prefer
+
+// Use express.json() middleware to parse JSON requests
+app.use(express.json());
+// Enable CORS for all routes
+app.use(cors());
+
+// Define a route to retrieve engagement metrics for a YouTube video
+app.post('/api', async (req, res) => {
+  try {
+    // Extract the video URL from the request body
+    const videoUrl = req.body.videoUrl;
+
+    // Extract the video ID from the URL
+    const videoId = getVideoIdFromUrl(videoUrl);
+
+    if (!videoId) {
+      return res.status(400).json({ error: 'Invalid YouTube video URL' });
+    }
+
+    // Replace 'YOUR_API_KEY' with your actual YouTube Data API key
+    const apiKey = 'AIzaSyCJDYmlIj_h8ptrnmkAOlNZz0LU67xU9_w';
+
+    // Fetch data from the YouTube Data API for the video
+    const videoApiUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoId}&key=${apiKey}`;
+    const videoResponse = await axios.get(videoApiUrl);
+    const videoData = videoResponse.data.items[0];
+
+    if (!videoData) {
+      return res.status(404).json({ error: 'Video not found' });
+    }
+
+    // Extract channel ID from video data
+    const channelId = videoData.snippet.channelId;
+
+    // Fetch data from the YouTube Data API for the channel to get subscriber count
+    const channelApiUrl = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`;
+    const channelResponse = await axios.get(channelApiUrl);
+    const channelData = channelResponse.data.items[0];
+
+    if (!channelData) {
+      return res.status(404).json({ error: 'Channel not found' });
+    }
+
+    // Extract engagement metrics
+    const subscriberCount = channelData.statistics.subscriberCount;
+    const likes = videoData.statistics.likeCount;
+    const comments = videoData.statistics.commentCount;
+    const views = videoData.statistics.viewCount;
+
+    // Get the video's uploaded date from the snippet
+    const uploadedDate = videoData.snippet.publishedAt;
+
+    // Get the video title from the snippet
+    const videoTitle = videoData.snippet.title;
+
+    // Earnings from the video 
+    const earnings = calculateEarnings(subscriberCount, likes, comments, views).toLocaleString();
+
+
+    // Return the engagement metrics and earning data as JSON 
+    res.json({
+      earnings,
+      subscriberCount,
+      likes,
+      comments,
+      views,
+      uploadedDate,
+      videoTitle
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Helper function to extract video ID from a YouTube URL
+function getVideoIdFromUrl(url) {
+  const match = url.match(/(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?feature=player_embedded&v=))([\w-]{11})/);
+  return match ? match[1] : null;
+}
+
+// Helper function to calculate earnings (replace with your formula)
+function calculateEarnings(subscriberCount, likes, comments, views) {
+  return Math.min(subscriberCount, views) + 10 * comments + 5 * likes;
+}
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
